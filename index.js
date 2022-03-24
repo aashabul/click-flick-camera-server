7ded34d07e98d16a9cb75477851e1df84c61e732
aashabul <aashabulimam@gmail.com>
2022-03-25 00:36:28 +0600

    delete wishlist added

--- index.js
+++ index.js
@@ -79,6 +79,14 @@ async function run() {
       res.json(deletedItem);
     });
 
+    //delete from wishlist
+    app.delete("/wishlist/:id", async (req, res) => {
+      const wishlistId = req.params.id;
+      query = { _id: ObjectId(wishlistId) };
+      const deleteWishlist = await wishlistCollection.deleteOne(query);
+      res.json(deleteWishlist);
+    });
+
     //post to wishlist
     app.post("/wishlist", async (req, res) => {
       const wishlist = req.body;
